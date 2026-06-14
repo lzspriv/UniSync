05e1f7689bf92006ed2cc64698f774b3ac34ef9e
Sam Liu <lzspriv@gmail.com>
2026-06-14 17:27:43 +0800

    feat: 新增總務處首頁最新消息

--- src/scraper.py
+++ src/scraper.py
@@ -486,7 +486,7 @@ def fetch_university_announcements(url, category_name, scraper_config=None):
                     continue
                 strip_title_suffix = scraper_config.get("strip_title_suffix")
                 if strip_title_suffix and link_text.endswith(strip_title_suffix):
-                    link_text = link_text[: -len(strip_title_suffix)].rstrip()
+                    link_text = link_text[: -len(strip_title_suffix)].rstrip(" .")
 
                 absolute_url = urljoin(url, link_tag.get('href'))
                 if absolute_url in seen_urls:
